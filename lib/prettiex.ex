adae4ed38563d30db84f9cc8d3c51a7d10849042
Eduardo Poernbacher <eduardo.poernbacher@thescore.com>
2023-02-02 14:18:54 -0300

    Initial commit

---
+++ lib/prettiex.ex
@@ -0,0 +1,18 @@
+defmodule Prettiex do
+  @moduledoc """
+  Documentation for `Prettiex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Prettiex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prettiex_test.exs
@@ -0,0 +1,8 @@
+defmodule PrettiexTest do
+  use ExUnit.Case
+  doctest Prettiex
+
+  test "greets the world" do
+    assert Prettiex.hello() == :world
+  end
+end
