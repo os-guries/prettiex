3c7bfa88c30d057785caeeb25eccfb86e832c080
Renan Vaz <renan.vaz@thescore.com>
2023-02-04 19:39:41 -0300

    Cleanup runner move stuff to ast

--- lib/prettiex/ast.ex
+++ lib/prettiex/ast.ex
@@ -19,6 +19,54 @@ defmodule Prettiex.AST do
     not is_nil(get(expr, target))
   end
 
+  def find_single_matches(
+        [pattern | patterns],
+        ast,
+        initial_matches \\ []
+      ) do
+    {_new_ast, matches} =
+      Macro.prewalk(ast, initial_matches, fn node, matches ->
+        if matches?(pattern.form, node) do
+          {node, [if(pattern.skip?, do: :skip_match, else: :match) | matches]}
+        else
+          {node, matches}
+        end
+      end)
+
+    case patterns do
+      [] -> matches
+      remaining -> find_single_matches(remaining, ast, matches)
+    end
+  end
+
+  def find_sibling_matches(patterns, ast, initial_matches \\ [])
+
+  def find_sibling_matches(
+        [p1, p2 | patterns],
+        ast,
+        initial_matches
+      ) do
+    {_new_ast, matches} =
+      ast
+      |> Z.zip()
+      |> Z.traverse(initial_matches, fn zipper, matches ->
+        sibling = Z.right(zipper)
+
+        if matches?(p1.form, Z.node(zipper)) and not is_nil(sibling) and
+             matches?(p2.form, Z.node(sibling)) do
+          {zipper, [:match | matches]}
+        else
+          {zipper, matches}
+        end
+      end)
+
+    find_sibling_matches(patterns, ast, matches)
+  end
+
+  def find_sibling_matches(_patterns, _ast, initial_matches) do
+    initial_matches
+  end
+
   def matches?({a, _, a_children}, {b, _, b_children}) do
     cond do
       a == b and is_nil(a_children) -> true
@@ -33,7 +81,7 @@ defmodule Prettiex.AST do
 
   defp children_matches?(a, b) do
     Enum.zip(a, b)
-    |> Enum.all?(fn {aa, bb} -> match?(aa, bb) end)
+    |> Enum.all?(fn {aa, bb} -> matches?(aa, bb) end)
   end
 
   defp maybe_node(input) do
--- lib/prettiex/runner.ex
+++ lib/prettiex/runner.ex
@@ -5,7 +5,6 @@ defmodule Prettiex.Runner do
   alias Prettiex.Issue
   alias Prettiex.Check.Sequence
   alias Prettiex.AST
-  alias Sourceror.Zipper, as: Z
 
   def run(module, ast) do
     check = module.spark_dsl_config[[:check]]
@@ -22,7 +21,7 @@ defmodule Prettiex.Runner do
   end
 
   defp interpret(check, %All{patterns: patterns}, ast) do
-    matches? = patterns |> find_single_matches(ast) |> Enum.all?(&(&1 == :match))
+    matches? = patterns |> AST.find_single_matches(ast) |> Enum.all?(&(&1 == :match))
 
     if matches? do
       [emit_issue!(check)]
@@ -32,7 +31,7 @@ defmodule Prettiex.Runner do
   end
 
   defp interpret(check, %Sequence{patterns: patterns}, ast) do
-    matches? = patterns |> find_sibling_matches(ast) |> Enum.any?(&(&1 == :match))
+    matches? = patterns |> AST.find_sibling_matches(ast) |> Enum.any?(&(&1 == :match))
 
     if matches? do
       [emit_issue!(check)]
@@ -43,54 +42,6 @@ defmodule Prettiex.Runner do
 
   defp interpret(_check, _node, _ast) do
     []
-  end
-
-  defp find_single_matches(
-         [pattern | patterns],
-         ast,
-         initial_matches \\ []
-       ) do
-    {_new_ast, matches} =
-      Macro.prewalk(ast, initial_matches, fn node, matches ->
-        if AST.matches?(pattern.form, node) do
-          {node, [if(pattern.skip?, do: :skip_match, else: :match) | matches]}
-        else
-          {node, matches}
-        end
-      end)
-
-    case patterns do
-      [] -> matches
-      remaining -> find_single_matches(remaining, ast, matches)
-    end
-  end
-
-  defp find_sibling_matches(patterns, ast, initial_matches \\ [])
-
-  defp find_sibling_matches(
-         [p1, p2 | patterns],
-         ast,
-         initial_matches
-       ) do
-    {_new_ast, matches} =
-      ast
-      |> Z.zip()
-      |> Z.traverse(initial_matches, fn zipper, matches ->
-        sibling = Z.right(zipper)
-
-        if AST.matches?(p1.form, Z.node(zipper)) and not is_nil(sibling) and
-             AST.matches?(p2.form, Z.node(sibling)) do
-          {zipper, [:match | matches]}
-        else
-          {zipper, matches}
-        end
-      end)
-
-    find_sibling_matches(patterns, ast, matches)
-  end
-
-  defp find_sibling_matches([_] = _patterns, _ast, initial_matches) do
-    initial_matches
   end
 
   defp emit_issue!(%{entities: [%Meta{} = meta | _]}) do
